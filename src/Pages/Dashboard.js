98491e7e5edf7cf2c29be7fd648bbe0dccd97b9b
Gameel-Allam <fadyallam8@gmail.com>
2022-10-31 18:34:46 +0200

    Adding a splash window to handle data loading waiting time

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Splash from './Components/Splash'
 
 const Dashboard = () => {
   return (
     <div>
-        Dashboard Underbuilding
+        <Splash></Splash>
     </div>
   )
 }
